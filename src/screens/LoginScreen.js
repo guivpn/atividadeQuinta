5728c7efb88d7caf78d30501cf3d778aea63b739
MarcoThulio1207 <marcothulio120799@gmail.com>
2025-05-20 21:10:25 -0300

    Classe de teste e function (método) vazio dentro da classe

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,71 @@
+
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+
+export default function LoginScreen(){
+  return(
+    <View style={styles.container}>
+      <Text style={styles.txtHome}>Validador de Senha</Text>
+
+      <Text style={styles.label}>Senha:</Text>
+      <TextInput
+      style={styles.input}
+      secureTextEntry
+      placeholder='Insira sua Senha!'
+      />
+
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.txtBtn}>Validar</Text>
+      </TouchableOpacity>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+    marginTop:50
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 16,
+    borderRadius: 5,
+  },
+  errorContainer: {
+    marginTop: 16,
+  },
+  error: {
+    color: 'red',
+    fontSize: 14,
+  },
+  txtHome:{
+    fontSize:20,
+    marginTop:-300,
+    alignItems:'center',
+
+  },
+  btn:{
+    marginTop:20,
+    backgroundColor:"red",
+    width:'50%',
+    height:'5%',
+    borderRadius:8
+  },
+  txtBtn:{
+    textAlign:'center',
+    padding:8,
+    fontWeight:'bold',
+    fontSize:15
+  }
+});
+
